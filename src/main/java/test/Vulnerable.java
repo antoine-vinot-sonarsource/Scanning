e9c57edc94360b5c34ab5db0d49f4f18b3b5000c
Antoine Vinot <antoine.vinot@sonarsource.com>
2022-12-14 12:08:37 +0100

    taint vul

--- src/main/java/test/Vulnerable.java
+++ src/main/java/test/Vulnerable.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -34,5 +36,15 @@ public class Vulnerable {
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private Path taintVulnerability(final String fileName) { //fileName is user controlled
+    Path parentPath = Paths.get("notUserControlledParameters");
+    Path requestedFilePath = Paths.get("notUserControlledParameters", fileName);
+    // security check against hacks like passing '../filename'
+    if ( ! parentPath.equals( requestedFilePath.getParent() ) ) {
+      throw new IllegalArgumentException("");
+    }
+    return requestedFilePath;
   }
 }
