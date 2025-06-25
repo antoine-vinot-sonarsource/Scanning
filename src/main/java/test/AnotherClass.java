6bd0a6913ce85d48469cd94b63ea6028d84ca3d1
antoine.vinot <antoine.vinot@sonarsource.com>
2025-06-25 09:50:19 +0200

    AnotherClass

---
+++ src/main/java/test/AnotherClass.java
@@ -0,0 +1,16 @@
+package test;
+
+public class AnotherClass {
+
+  private String key;
+
+  private String Stuff;
+
+  public AnotherClass(String key) {
+    this.key = key;
+  }
+
+  public String getName() {
+    return key;
+  }
+}
