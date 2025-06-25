0b45b5a9de3f69acf4e5cf2323e4c56801d59ec6
antoine.vinot <antoine.vinot@sonarsource.com>
2025-06-25 08:56:32 +0200

    DummyClass

---
+++ src/main/java/test/DummyClass.java
@@ -0,0 +1,16 @@
+package test;
+
+public class DummyClass {
+
+  private String name;
+
+  private String Issue;
+
+  public DummyClass(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
